1aec3db026083b5e6bed055111d27b7982e2c9b3
Michael Bayha <kg7pnh@arrl.net>
2022-01-15 11:48:28 -0800

    Completed assignment

---
+++ js/program.js
@@ -0,0 +1,45 @@
+let userInput = [];
+let modifiedInput = [];
+
+let getInput = document.querySelector("#getInput"),
+    startSection = document.querySelector("#startSection"),
+    ulOriginal = document.querySelector("#ulOriginal"),
+    updateInput = document.querySelector("#updateInput"),
+    endSection = document.querySelector("#endSection"),
+    ulConverted = document.querySelector("#ulConverted")
+
+getInput.addEventListener("click", getUserInput, false)
+updateInput.addEventListener("click", updateUserInput, false)
+
+function getUserInput() {
+    for (let i = 0; i < 3; i++) {
+        userInput[i] = prompt("Please enter a word");
+        createListItem("#ulOriginal", userInput[i]);
+    }
+    getInput.style.visibility = "hidden";
+    updateInput.style.visibility = "visible";
+}
+
+function updateUserInput() {
+    for (let i = 0; i < userInput.length; i++) {
+        let newString = swapFirstAndLast(userInput[i]);
+        createListItem("#ulConverted", newString);
+    }
+    startSection.style.display = "none";
+    endSection.style.display = "block";
+    updateInput.style.visibility = "hidden";
+}
+
+function createListItem(listItemId, listItemTest) {
+    let ulTarget = document.querySelector(listItemId);
+    let inputListItem = document.createElement('li');
+    inputListItem.appendChild(document.createTextNode(listItemTest));
+    ulTarget.appendChild(inputListItem);
+}
+
+function swapFirstAndLast(inputString) {
+    let reversedString = inputString.charAt(inputString.length - 1) +
+        inputString.substring(1, inputString.length - 1) +
+        inputString.charAt(0);
+    return reversedString;
+}
